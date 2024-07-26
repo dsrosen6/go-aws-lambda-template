abba500839715ef3af8cf7ac7e5cd355642e322d
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2024-07-26 18:14:14 -0500

    add some comments, re-order functions

--- main.go
+++ main.go
@@ -6,22 +6,26 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
+// The data that is passed to the Lambda function
+type Event struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
 
-type MyResponse struct {
+// The data that is returned from the Lambda function
+type Response struct {
 	Message string `json:"answer"`
 }
 
-func HandleLambdaEvent(event *MyEvent) (*MyResponse, error) {
+// Entry point, no action needed
+func main() {
+	lambda.Start(HandleLambdaEvent)
+}
+
+// Lambda function handler - main logic starts here
+func HandleLambdaEvent(event *Event) (*Response, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
 	}
-	return &MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
-}
-
-func main() {
-	lambda.Start(HandleLambdaEvent)
+	return &Response{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
